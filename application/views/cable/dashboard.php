dd58c35d2bb56360efe0c087d006b1c92514f8e4
sanjoym152 <sanjoym152@gmail.com>
2018-05-07 09:44:15 +0530

    dashboard

--- application/views/cable/dashboard.php
+++ application/views/cable/dashboard.php
@@ -81,7 +81,7 @@
 		<!-- Payment Modal -->
 		<div id="payment" class="modal fade" role="dialog">
 			<div class="modal-dialog modal-lg">
-			<!-- Modal content-->
+				<!-- Modal content-->
 				<div class="modal-content">
 					<form id="payment_form" method="post" action="<?php echo base_url('cable/payment/add_payment')?>">
 						<div class="modal-header">
@@ -165,7 +165,6 @@
 						<div class="modal-footer">
 							<div class="col-md-12">
 								<button type="submit" id="print_bill" class="btn btn-success payment_btn"><i class="fa fa-print" aria-hidden="true"></i> Payment & Print</button>
-								<!--<button type="button" class="btn btn-success payment_btn" id="make_payment"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Make Payment</button>-->
 							</div>
 						</div>
 					</form>
@@ -181,242 +180,296 @@
 		<script type="text/javascript" src="js/jquery.validate.js"></script>
 		
 		<script>
-			$( function() {
-				
-				$( "#autocomplete" ).autocomplete({
-					source: function( request, response ) {
-						$.ajax({
-							url:'<?php echo base_url('cable/home/autocomplete')?>',
-							method:'post',
-							dataType: "json",
-							data: {
-								keyword: request.term
-							},
-							success: function( data ) {
-								response( data );
-							}
-						});
-					},
-					minLength: 2,
-					select: function( event, ui ) {
-						$('.payment_div').hide();
-						$('.before_select').show();
-						$('.account_info').hide();
-						$('.year').val('');
-						$('.loader').show();
-						$('.loader-inner').show();
-						event.preventDefault(); // <--- Prevent the value from being inserted.
-						$('#autocomplete').val(ui.item.label);
-						$.ajax({
-							url:'<?php echo base_url('cable/home/get_customer_details')?>',
-							method:'post',
-							dataType: "json",
-							data: {
-								customer_id: ui.item.value
-							},
-							success: function( result ) {
-								$('.loader').hide();
-								$('.loader-inner').hide();
-								//$('.before_select').hide();
-								$('.basic_info').html(result.template);
-								$('.payment_div').slideDown();
-								console.log(result);
-							}, 
-							error: function(err){
-								console.log(err.responseText);
-							}
-						});
-					}
-				});
-				
-				$('.year').change(function(){
-					//console.log($(this).val());
-					var year = $(this).val();
-					var cust_id = $('.edit_cust').data('id');
-					console.log(cust_id);
+		$( function() {
+			$( "#autocomplete" ).autocomplete({
+				source: function( request, response ) {
 					$.ajax({
-						url:'<?php echo base_url('cable/home/get_payment_info');?>',
+						url:'<?php echo base_url('cable/home/autocomplete')?>',
 						method:'post',
-						data:{
-							customer_id:cust_id,
-							year:year
+						dataType: "json",
+						data: {
+							keyword: request.term
 						},
-						dataType:'json',
-						success:function(result){
-							$('.before_select').hide();
-							$('.account_info').show();
-							if(result.error){
-								console.log(result.error);
-							} else{
-								console.log(result);
-								$('.account_info').html(result.template);
-							}
-						},
-						error:function(err){
-							console.log(err);
+						success: function( data ) {
+							response( data );
 						}
 					});
-				});
-				
-				/*
-				* @ This function is used for add payment.
-				*/
-				$('body').on('click','.add_payment',function(){
-					var customer_id = $(this).data('id');
-					var payment_id = $(this).data('payment_id');
+				},
+				minLength: 2,
+				select: function( event, ui ) {
+					$('.payment_div').hide();
+					$('.before_select').show();
+					$('.account_info').hide();
+					$('.year').val('');
 					$('.loader').show();
 					$('.loader-inner').show();
+					event.preventDefault(); // <--- Prevent the value from being inserted.
+					$('#autocomplete').val(ui.item.label);
 					$.ajax({
-						url:'<?php echo base_url('cable/customers/get_customer_data')?>',
+						url:'<?php echo base_url('cable/home/get_customer_details')?>',
 						method:'post',
-						data:{
-							customer_id:customer_id
+						dataType: "json",
+						data: {
+							customer_id: ui.item.value
 						},
-						dataType:'json',
-						success:function(result){
-							$('.customer_payment_info').html(result.customer_info);
-							$('#total_due').val(result.customer.balance>=0?result.customer.balance:0);
-							if(result.customer.payment_status==1){
-								$('#pack_amount').val(result.customer.tot_amount);
-							}
-							$('#package_id').val(result.customer.package_id);
-							$('#customer_id').val(result.customer.customer_id);
-							$('#payment_id').val(payment_id);
-							setTimeout(function(){
-								$('.loader').hide();
-								$('.loader-inner').hide();
-								$('#payment').modal('show');
-							}, 1000);
-						},
-						error:function(error){
-							console.log(error.responseText);
+						success: function( result ) {
+							$('.loader').hide();
+							$('.loader-inner').hide();
+							//$('.before_select').hide();
+							$('.basic_info').html(result.template);
+							$('.payment_div').slideDown();
+						}, 
+						error: function(err){
+							console.log(err.responseText);
 						}
 					});
-				});
-				
-				
-				// Payment calculation 
-				$('body').on('keyup','#discount_amount',function(){
-					if($(this).val()!=""){
-						$('#discount_type').addClass('required');
-					}else{
-						$('#discount_type').removeClass('required');
-					}
-					var payment_total = $('#payment_total').val();
-					$('#discount_total').val(0.00);
-					var discount_amount = $('#discount_amount').val();
-					var discount_type = $('#discount_type').val();
-					var discount_total = 0.00;
-					var total_due = $('#total_due').val();
-					var pack_amount = $('#pack_amount').val();
-					//alert(discount_type);
-					if(discount_type==1){
-						discount_total = ((parseInt(total_due)*parseInt(discount_amount))/100);
-					}else if(discount_type==2){
-						discount_total = parseInt(discount_amount);
-					}
-					$('#discount_total').val(discount_total);
-					net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
-					$('#net_due').val(net_due);
-				});
-				
-				$('body').on('change','#discount_type',function(){
-					if($(this).val()!=""){
-						$('#discount_amount').addClass('required');
-					}else{
-						$('#discount_amount').removeClass('required');
-					}
-					$('#discount_total').val(0.00);
-					var payment_total = $('#payment_total').val();
-					var discount_amount = $('#discount_amount').val();
-					var discount_type = $('#discount_type').val();
-					var discount_total = 0.00;
-					var total_due = $('#total_due').val();
-					var pack_amount = $('#pack_amount').val();
-					//alert(discount_type);
-					if(discount_type==1){
-						discount_total = ((parseInt(total_due)*parseInt(discount_amount))/100);
-					}else if(discount_type==2){
-						discount_total = parseInt(discount_amount);
-					}
-					$('#discount_total').val(discount_total);
-					net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
-					$('#net_due').val(net_due);
-				});
-				
-				$('#payment_total').keyup(function(){
-					var total_due = $('#total_due').val();
-					var pack_amount = $('#pack_amount').val();
-					var discount_total = $('#discount_total').val();
-					var payment_total = $('#payment_total').val();
-					var net_due = 0;
-					net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
-					$('#net_due').val(net_due);
-				});
-				
-				$('.datepicker').datepicker({
-					dateFormat:'yy-mm-dd'
-				});
-				$('#payment_form').validate({
-					submitHandler: function (form) {
-						form.submit();
-					}
-				});
-				
-				/* Delete payment */
-				$('body').on('click', '.delete_payment', function(){
-					if(confirm('Are you sure?')){
-						var payment_id = $(this).data('id');
-						var customer_id = $(this).data('customer_id');
-						var year = $('.year').val();
-						$.ajax({
-							method:'post',
-							url: '<?php echo base_url('cable/payment/delete_payment')?>',
-							data:{ payment_id: payment_id, customer_id: customer_id, year: year},
-							dataType:'json',
-							success:function(result){
-								console.log(result);
-								$('.account_info').html(result.template);
-							},
-							error: function(err){
-								console.log(err.responseText);
-							}
-						});
-					}
-				});
-				/* Delete payment */
-				
-				
-				$('body').on('click', '.edit_payment', function(){
-					var payment_id = $(this).data('id');
-					var customer_id = $(this).data('customer_id');
-					$.ajax({
-						url:'<?php echo base_url('cable/home/get_payment_data')?>',
-						method:'post',
-						data:{
-							payment_id:payment_id,
-							customer_id:customer_id
-						},
-						dataType:'json',
-						success:function(result){
-							$('#edit_payment').modal('show');
-							console.log(payment_id);
-							$('.payment_id').val(payment_id);
-							$('.customer_id').val(customer_id);
-							$('.month_of').val(result.payment_data.month_of);
-							$('.payment_date').val(result.payment_data.payment_date);
-							$('.payment_total').val(result.payment_data.payment_total);
-							$('.discount_total').val(result.payment_data.discount_total);
-							$('.discount_amount').val(result.payment_data.discount_in);
-							$('.discount_type').val(result.payment_data.discount_type);
-							$('.collector').val(result.payment_data.staff_id);
-						},
-						error:function(error){
-							console.log(error.responseText);
+				}
+			});
+			
+			$('.year').change(function(){
+				//console.log($(this).val());
+				var year = $(this).val();
+				var cust_id = $('.edit_cust').data('id');
+				console.log(cust_id);
+				$.ajax({
+					url:'<?php echo base_url('cable/home/get_payment_info');?>',
+					method:'post',
+					data:{
+						customer_id:cust_id,
+						year:year
+					},
+					dataType:'json',
+					success:function(result){
+						$('.before_select').hide();
+						$('.account_info').show();
+						if(result.error){
+							console.log(result.error);
+						} else{
+							$('.account_info').html(result.template);
 						}
-					});
+					},
+					error:function(err){
+						console.log(err);
+					}
 				});
 			});
+			
+			/*
+			* @ This function is used for add payment.
+			*/
+			$('body').on('click','.add_payment',function(){
+				var customer_id = $(this).data('id');
+				var payment_id = $(this).data('payment_id');
+				$('.loader').show();
+				$('.loader-inner').show();
+				$.ajax({
+					url:'<?php echo base_url('cable/customers/get_customer_data')?>',
+					method:'post',
+					data:{
+						customer_id:customer_id
+					},
+					dataType:'json',
+					success:function(result){
+						$('.customer_payment_info').html(result.customer_info);
+						$('#total_due').val(result.customer.balance>=0?result.customer.balance:0);
+						if(result.customer.payment_status==1){
+							$('#pack_amount').val(result.customer.tot_amount);
+						}
+						$('#package_id').val(result.customer.package_id);
+						$('#customer_id').val(result.customer.customer_id);
+						$('#payment_id').val(payment_id);
+						setTimeout(function(){
+							$('.loader').hide();
+							$('.loader-inner').hide();
+							$('#payment').modal('show');
+						}, 1000);
+					},
+					error:function(error){
+						console.log(error.responseText);
+					}
+				});
+			});
+			
+			
+			// Payment calculation for add payment topup
+			$('body').on('keyup','#discount_amount',function(){
+				if($(this).val()!=""){
+					$('#discount_type').addClass('required');
+				}else{
+					$('#discount_type').removeClass('required');
+				}
+				var payment_total = $('#payment_total').val();
+				$('#discount_total').val(0.00);
+				var discount_amount = $('#discount_amount').val();
+				var discount_type = $('#discount_type').val();
+				var discount_total = 0.00;
+				var total_due = $('#total_due').val();
+				var pack_amount = $('#pack_amount').val();
+
+				if(discount_type==1){
+					discount_total = ((parseInt(total_due)*parseInt(discount_amount))/100);
+				}else if(discount_type==2){
+					discount_total = parseInt(discount_amount);
+				}
+				$('#discount_total').val(discount_total);
+				net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
+				$('#net_due').val(net_due);
+			});
+			
+			$('body').on('change','#discount_type',function(){
+				if($(this).val()!=""){
+					$('#discount_amount').addClass('required');
+				}else{
+					$('#discount_amount').removeClass('required');
+				}
+				$('#discount_total').val(0.00);
+				var payment_total = $('#payment_total').val();
+				var discount_amount = $('#discount_amount').val();
+				var discount_type = $('#discount_type').val();
+				var discount_total = 0.00;
+				var total_due = $('#total_due').val();
+				var pack_amount = $('#pack_amount').val();
+				
+				if(discount_type==1){
+					discount_total = ((parseInt(total_due)*parseInt(discount_amount))/100);
+				}else if(discount_type==2){
+					discount_total = parseInt(discount_amount);
+				}
+				$('#discount_total').val(discount_total);
+				net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
+				$('#net_due').val(net_due);
+			});
+			
+			$('#payment_total').keyup(function(){
+				var total_due = $('#total_due').val();
+				var pack_amount = $('#pack_amount').val();
+				var discount_total = $('#discount_total').val();
+				var payment_total = $('#payment_total').val();
+				var net_due = 0;
+				net_due = (parseFloat(total_due)-parseFloat(discount_total))-parseFloat(payment_total);
+				$('#net_due').val(net_due);
+			});
+			
+			$('.datepicker').datepicker({
+				dateFormat:'yy-mm-dd'
+			});
+			$('#payment_form').validate({
+				submitHandler: function (form) {
+					form.submit();
+				}
+			});
+			
+			/* Delete payment */
+			$('body').on('click', '.delete_payment', function(){
+				if(confirm('Are you sure?')){
+					var payment_id = $(this).data('id');
+					var customer_id = $(this).data('customer_id');
+					var year = $('.year').val();
+					$.ajax({
+						method:'post',
+						url: '<?php echo base_url('cable/payment/delete_payment')?>',
+						data:{ payment_id: payment_id, customer_id: customer_id, year: year},
+						dataType:'json',
+						success:function(result){
+							$('.account_info').html(result.template);
+						},
+						error: function(err){
+							console.log(err.responseText);
+						}
+					});
+				}
+			});
+			/* Delete payment */
+			
+			// Edit payment
+			$('body').on('click', '.edit_payment', function(){
+				var payment_id = $(this).data('id');
+				var customer_id = $(this).data('customer_id');
+				$.ajax({
+					url:'<?php echo base_url('cable/home/get_payment_data')?>',
+					method:'post',
+					data:{
+						payment_id:payment_id,
+						customer_id:customer_id
+					},
+					dataType:'json',
+					success:function(result){
+						$('#edit_payment').modal('show');
+						$('.payment_id').val(payment_id);
+						$('.customer_id').val(customer_id);
+						$('.month_of').val(result.payment_data.month_of);
+						$('.payment_date').val(result.payment_data.payment_date);
+						$('.payment_total').val(result.payment_data.payment_total);
+						$('.discount_total').val(result.payment_data.discount_total);
+						$('.discount_amount').val(result.payment_data.discount_in);
+						$('.discount_type').val(result.payment_data.discount_type);
+						$('.collector').val(result.payment_data.staff_id);
+					},
+					error:function(error){
+						console.log(error.responseText);
+					}
+				});
+			});
+			
+			// Discount calculation for edit popup
+			$('body').on('keyup','.discount_amount',function(){
+				if($(this).val()!=""){
+					$('.discount_type').addClass('required');
+				}else{
+					$('.discount_type').removeClass('required');
+				}
+				var payment_total_edit = $('.payment_total').val();
+				$('.discount_total').val(0.00);
+				var discount_amount_edit = $('.discount_amount').val();
+				var discount_type_edit = $('.discount_type').val();
+				var discount_total_edit = 0.00;
+				var total_due_edit = $('.total_due').val();
+				var pack_amount_edit = $('.pack_amount').val();
+
+				if(discount_type_edit == 1){
+					discount_total_edit = ((parseInt(total_due_edit)*parseInt(discount_amount_edit))/100);
+				}else if(discount_type_edit==2){
+					discount_total_edit = parseInt(discount_amount_edit);
+				}
+				$('.discount_total').val(discount_total_edit);
+				net_due_edit = (parseFloat(total_due_edit)-parseFloat(discount_total_edit))-parseFloat(payment_total_edit);
+				$('.net_due').val(net_due_edit);
+			});
+			
+			$('body').on('change','.discount_type',function(){
+				if($(this).val()!=""){
+					$('.discount_amount').addClass('required');
+				}else{
+					$('.discount_amount').removeClass('required');
+				}
+				$('.discount_total').val(0.00);
+				var payment_total_edit = $('.payment_total').val();
+				var discount_amount_edit = $('.discount_amount').val();
+				var discount_type_edit = $('.discount_type').val();
+				var discount_total_edit = 0.00;
+				var total_due_edit = $('.total_due').val();
+				var pack_amount_edit = $('.pack_amount').val();
+				
+				if(discount_type_edit==1){
+					discount_total_edit = ((parseInt(total_due_edit)*parseInt(discount_amount_edit))/100);
+				}else if(discount_type_edit==2){
+					discount_total_edit = parseInt(discount_amount_edit);
+				}
+				$('.discount_total').val(discount_total_edit);
+				net_due_edit = (parseFloat(total_due_edit)-parseFloat(discount_total_edit))-parseFloat(payment_total_edit);
+				$('.net_due').val(net_due_edit);
+			});
+			
+			$('.payment_total').keyup(function(){
+				var total_due_edit = $('.total_due').val();
+				var pack_amount_edit = $('.pack_amount').val();
+				var discount_total_edit = $('.discount_total').val();
+				var payment_total_edit = $('.payment_total').val();
+				var net_due_edit = 0;
+				net_due_edit = (parseFloat(total_due_edit)-parseFloat(discount_total_edit))-parseFloat(payment_total_edit);
+				$('.net_due').val(net_due_edit);
+			});
+		});
 		</script>
 		
 		<!-- Edit Payment Modal -->
@@ -438,7 +491,6 @@
 									<div class="your-mail">
 										<label for="exampleInputEmail1">Description</label>
 										<input type="text" name="month_of" class="month_of required form-control" placeholder="Enter description of month.">
-										<span class="month_of_error"></span>
 									</div>
 								</div>
 								
@@ -484,7 +536,6 @@
 											<option value="<?php echo @$row['staff_id'];?>"><?php echo @$row['staff_name'];?></option>
 											<?php } ?>
 										</select>
-										<span class="collector_error"></span>
 									</div>
 								</div>
 								<div class="clearfix"></div>
